93e24b82a6f2d2158bb991afe036e4c250d8eb01
electroo0 <90555965+amitjimiwal@users.noreply.github.com>
2025-04-18 22:50:25 +0530

    api security headers

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,35 @@ const nextConfig: NextConfig = {
     }
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: `${process.env.NEXT_PUBLIC_APP_URL}` },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self'"
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          }
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
